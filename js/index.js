b84d800dbe073119cebc4161a5c844afb743bd07
EduardoDamaceno <edubiju@hotmail.com>
2022-02-12 13:08:49 -0300

    Switch Theme complete

--- js/index.js
+++ js/index.js
@@ -23,3 +23,37 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+function initDarkMode(){
+  const theme = document.querySelector('#theme'),
+        moon = document.querySelector('.uil-moon'),
+        sun = document.querySelector('.uil')
+        body = document.querySelector('body'),
+        header = document.querySelector('header'),
+        experience = document.querySelector('#experience')
+
+
+  function changeTheme(){
+    if (theme.classList.contains('uil-moon')){
+      theme.classList.remove('uil-moon')
+      theme.classList.add('uil-sun')
+      body.classList.add('night-mode')
+      header.classList.add('night-mode')
+      experience.classList.add('night-mode')
+    } else {
+      theme.classList.remove('uil-sun')
+      theme.classList.add('uil-moon')
+      body.classList.remove('night-mode')
+      header.classList.remove('night-mode')
+      experience.classList.remove('night-mode')
+    }
+
+    
+  
+  }
+  theme.addEventListener('click', changeTheme)
+
+}
+
+initDarkMode()
