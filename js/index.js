e5c687b33e6e824741305a037953f66c3b62b59c
EduardoDamaceno <edubiju@hotmail.com>
2022-02-12 21:42:26 -0300

    Adjustments

--- js/index.js
+++ js/index.js
@@ -1,40 +1,32 @@
+function initShowMenu(){
 const navMenu = document.querySelector("#nav-menu"),
-      navToggle = document.querySelector("#nav-apps"),
-      navClose = document.querySelector("#nav-close")
+        navToggle = document.querySelector("#nav-apps"),
+        navClose = document.querySelector("#nav-close")
 
-if(navToggle){
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu')
-  })
+  if(navToggle){
+    navToggle.addEventListener('click', () => {
+      navMenu.classList.add('show-menu')
+    })
+  }
+
+  if(navClose){
+    navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu')
+    }) 
+  }
 }
 
-if(navClose){
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu')
-  }) 
-}
-
-
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
+initShowMenu()
 
 function initDarkMode(){
   const theme = document.querySelector('#theme'),
-        moon = document.querySelector('.uil-moon'),
-        sun = document.querySelector('.uil')
         body = document.querySelector('body'),
         header = document.querySelector('header'),
         experience = document.querySelector('#experience')
 
 
   function changeTheme(){
+
     if (theme.classList.contains('uil-moon')){
       theme.classList.remove('uil-moon')
       theme.classList.add('uil-sun')
@@ -48,12 +40,19 @@ function initDarkMode(){
       header.classList.remove('night-mode')
       experience.classList.remove('night-mode')
     }
-
-    
-  
   }
-  theme.addEventListener('click', changeTheme)
 
+  theme.addEventListener('click', changeTheme)
 }
 
 initDarkMode()
+
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
